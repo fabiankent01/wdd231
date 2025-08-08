31439edd32fe0aa7543afdab05f5f1843a0f7e29
fabiankent01 <fabiankent01@gmail.com>
2025-08-08 16:28:37 +0100

    new

---
+++ final/scripts/nav.js
@@ -0,0 +1,11 @@
+const menuToggle = document.getElementById('mobile-menu');
+        const nav = document.getElementById('nav');
+        const closeMenu = document.getElementById('close-menu');
+    
+        menuToggle.addEventListener('click', () => {
+            nav.classList.add('show-menu');
+        });
+    
+        closeMenu.addEventListener('click', () => {
+            nav.classList.remove('show-menu'); 
+        });
